cd3a9a2805c3e19789ab79237effad19b90ac8d7
Damian Medarov <d.medarov@me.com>
2025-01-06 15:21:01 +0000

    test1

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World Дамян!"
+    return "Hello World DAmian!"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
